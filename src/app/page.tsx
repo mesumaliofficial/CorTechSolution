0c8630d08bf7bb685641b4808b7027a52a5d70f9
Mesum Ali <syedmesumjaffary@gmail.com>
2025-04-09 16:04:32 +0500

    navbar completely responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,24 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+      <main className="grid grid-cols-2 h-[90vh] justify-between p-16">
+        <div className="text-black space-y-4 items-center justify-center flex flex-col">
+          <h1 className="text-5xl font-bold text-gray-900">
+            Redefining Your <span className="text-blue-500">Digital Future</span> with CorTech IT Solutions
+          </h1>
+          <p className="mt-4 text-lg text-gray-700">
+            Custom Software Development | Web & Mobile Solutions | Digital Transformation
+            Empowering Innovation for a Smarter Tomorrow
+          </p>
+        </div>
+        <div 
+          className="relative bg-cover bg-center h-full rounded-lg"
+          style={{ backgroundImage: "url('/images/banner.webp')" }}
+        >
+        </div>
+      </main>
+    </>
   );
 }
